b85c67282929bdf44171be6daf5c87134929b938
Nathan3-14 <nathan.watson.172@gmail.com>
2024-03-04 22:02:26 +0000

    Addded model and templates

--- forum_site/forum/models.py
+++ forum_site/forum/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=30)
+    post_body = models.CharField(max_length=300)
+    post_pub_date = models.DateTimeField("Date Published")
+
+    def __str__(self):
+        return self.post_title
+
+    def was_recent(self):
+        return self.post_pub_date >= timezone.now() - datetime.timedelta(days=1)
--- forum_site/forum/views.py
+++ forum_site/forum/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello! You are at the index for forum")
+    latest_post_list = Post.objects.order_by("-post_pub_date")[:5]
+    context = {
+        "latest_post_list": latest_post_list
+    }
+    return render(request, "forum/index.html", context)
+
+def post_view(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "forum/post.html", {"post": post})
