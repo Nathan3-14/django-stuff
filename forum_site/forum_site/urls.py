a0ae04c47364b19130adb80e66dcf8e979f534de
Nathan3-14 <nathan.watson.172@gmail.com>
2024-03-07 21:07:13 +0000

    Reformatted

--- forum_site/forum_site/urls.py
+++ forum_site/forum_site/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("index.urls")),
     path("forum/", include("forum.urls")),
+    path("somewhere/", include("somewhere.urls")),
     path("admin/", admin.site.urls)
 ]
